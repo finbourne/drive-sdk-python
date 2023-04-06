e73530c6d631d297485897805e92a7d7dcc970c3
daniel-ilyin <124591622+daniel-ilyin@users.noreply.github.com>
2023-04-06 15:36:33 +0100

    CTECH-2172: (#12)
    * CTECH-2172:
    Add automatic content length for file upload in lusid drive tools
    
    * CTECH-2172:
     Add automatic content length for file upload in lusid drive tools
    
    * CTECH-2172: automatic content length file upload

--- sdk/lusid_drive/utilities/lusid_drive_retry.py
+++ sdk/lusid_drive/utilities/lusid_drive_retry.py
@@ -5,7 +5,6 @@ from lusid_drive import ApiException
 
 
 def lusid_drive_retry(fn):
-
     @functools.wraps(fn)
     def __retry(*args, **kwargs):
 
@@ -14,13 +13,17 @@ def lusid_drive_retry(fn):
             retries = 3
 
         tries = 0
-        while tries < retries:
+        while tries < retries+1:
             try:
                 return fn(*args, **kwargs)
             except ApiException as ex:
 
                 tries += 1
-                retry_after = ex.headers.get("Retry-After")
+
+                retry_after = ex.headers.get("Retry-After") if ex.headers else None
+
+                if ex.status == 423:
+                    retry_after = 10
 
                 # have done max number of retries
                 if tries == retries:
@@ -28,6 +31,13 @@ def lusid_drive_retry(fn):
 
                 # try after delay
                 elif retry_after is not None:
+
+                    if not isinstance(retry_after, float):
+                        try:
+                            retry_after = float(retry_after)
+                        except ValueError:
+                            raise ValueError(f"invalid Retry-After header value: {retry_after}")
+
                     sleep(retry_after)
 
                 # no retry header
